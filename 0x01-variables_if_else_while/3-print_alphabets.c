7244478a3d2be26acdaf77b4906e1c5d70082f26
EmilioCliff <emiliocliff@gmail.com>
2023-02-18 05:37:29 +0300

    uploaded tasks

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+#include <stdio.h>
 /*
 *main - Program that prints lowercase then uppercase alphabets
 *Return: o
@@ -19,4 +33,21 @@ int main(void)
 	putchar("%c", m);
 }
 	return(0);
+}
+	/*prints a - z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		++CH;
+	}
+	putchar('\n');
+
+	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
-/*
-*main - Print alphabets except q and e
-*Return: 0
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters except q and e
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	int ch;
-	
-	for(ch ='a'; ch<=d; ch++)
-{
-	putchar ch;
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			++ch;
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
 }
-	for(
