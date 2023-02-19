ce90374a7da66b7769dd4e02e1a09b9f1ea96cc9
EmilioCliff <emiliocliff@gmail.com>
2023-02-19 13:03:40 +0300

    changed some permissions

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,52 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Prints alphabet in lower then uppercase
  *
- * Description: print alphabet in lowercase then in uppercase
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: 0
+ */
 int main(void)
 {
-	char ch = 'a';
-	char CH = 'A';
-#include <stdio.h>
-/*
-*main - Program that prints lowercase then uppercase alphabets
-*Return: o
-*/
-int main(void)
-{
-	int i, m;
+		char ch = 'a';
+		char CH = 'A';
 
-	i = 97;
-	m = 65;
-	for (i = 97; i <= 122; i++)
-{
-	putchar("%c", i);
-}
-	for (m = 65; i <= 90; m++)
-{
-	putchar("%c", m);
-}
-	return (0);
-}
-	/*prints a - z*/
-	while (ch <= 'z')
-	{
-		putchar(ch);
-		++ch;
-	}
-
-	/*print A - Z*/
-	while (CH <= 'Z')
-	{
-		putchar(CH);
-		++CH;
-	}
-	putchar('\n');
-
-	return (0);
+		while (ch <= 'z')
+		{
+			putchar (ch);
+			ch++;
+		}
+		while (CH <= 'Z')
+		{
+			putchar (CH);
+			CH++;
+		}
+		return (0);
 }
