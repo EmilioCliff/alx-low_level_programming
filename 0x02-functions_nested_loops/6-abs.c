1d365a828fcc3deb19c2e292d334093c5d0389a3
EmilioCliff <emiliocliff@gmail.com>
2023-02-21 22:01:09 +0300

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - find the absolute value of a number
+ * @int - the interger to be calculated
+ * Return: abs
+ */
+int _abs(int)
+{
+	int abs;
+	if (abs < 0)
+		abs = abs * (-1);
+	return (abs);
+}
+
