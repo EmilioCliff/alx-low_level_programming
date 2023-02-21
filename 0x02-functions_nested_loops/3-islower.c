50b2143c9cc3568a3148cbbe6048dfb91b59934b
EmilioCliff <emiliocliff@gmail.com>
2023-02-21 20:53:07 +0300

    deleted trailing spaces

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,19 @@
 #include "main.h"
+#include <ctype.h>
 /**
  * _islower - searches for lowercase in a character
+ * @c: takes the character to be searched
  *
  * Return: 1 if c is lowercase, otherwise 0
  */
 int _islower(int c)
 {
-	if (c <= 'a' && c <= 'z');
+	if (c <= 'a' && c <= 'z')
+	{
 		return (1);
-	else return (0);
+	}
+	else
+	{
+		return (0);
+	}
 }
