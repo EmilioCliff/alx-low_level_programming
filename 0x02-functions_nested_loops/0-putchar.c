bbb164ad2973b4ece8d9f7f224525f6ff2bccdc5
EmilioCliff <emiliocliff@gmail.com>
2023-02-21 18:06:40 +0300

    My first header file with one task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description - print some strings using my own header file
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char string = "_putchar";
+
+	_putchar(string);
+	return (0);
+}
