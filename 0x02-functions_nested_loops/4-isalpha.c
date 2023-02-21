3636f6ff40fcedf4b2b2f51f1a65a0f7b0bf9dc4
EmilioCliff <emiliocliff@gmail.com>
2023-02-21 21:25:33 +0300

    added paretheses

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
  * _isalpha - find if a character is a letter
- * @c: input data
+ * @c: data to be checked
  * Return: 1 if c is a letter, lowercase or uppercase otherwise 0
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
 
