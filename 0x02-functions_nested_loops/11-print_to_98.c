d9b5911cb4726724f5b2c7c5629eef04ca62f328
EmilioCliff <emiliocliff@gmail.com>
2023-02-22 00:28:27 +0300

    added else statement

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
+ * print_to_98 - prints natural number from n to 98
+ * n: the beginning point
  */
 void print_to_98(int n)
 {
-	printf("%d, ", n);
 	if (n < 98)
+	{
+		printf("%d, ", n);
 	while (n < 98)
 	{
-		printf("%d, ", (n +1));
+		printf("%d, ", (n + 1));
+	}
+	printf("98");
 	}
 	else if (n > 98)
 	{
+		printf("%d, ", n);
 		while (n > 98)
 		{
 			printf("%d, ", (n - 1));
 		}
-		else
+	}
+		printf("98");
+		else 
 		{
-			printf("98");
+			printf("%d", n);
+		}
 }
-
-
-
