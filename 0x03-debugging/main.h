e837101bddc4b314b3d3f14a66d74e4e44ecc053
EmilioCliff <emiliocliff@gmail.com>
2023-02-23 17:09:13 +0300

    Added function to header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,5 +3,6 @@
 #include<stdio.h>
 int main(void);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int ,int , int)
+int convert_day(int month, int day);
+void print_remaining_days(int month,int day, int year);
 #endif
