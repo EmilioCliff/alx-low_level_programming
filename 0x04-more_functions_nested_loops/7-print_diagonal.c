b40719c33b94fccf80618fabacc185bc36d54164
EmilioCliff <emiliocliff@gmail.com>
2023-02-28 16:20:20 +0300

    fix on task 7 and 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,22 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int rowno, colomno;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (postn = 1; postn <= n; ++postn)
+		for (colomno = 1; colomno <= n; ++colomno)
 		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
+			for (rowno = 1; colomno <= n; ++colomno)
+			{
+				if (rowno == colomno)
+					putchar(92);
+				else
+					putchar(' ');
+			}
+			putchar('\n');
 		}
 	}
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,10 +12,19 @@ void print_square(int size)
 {
 	int row, column;
 
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
 	for (row = 1; row <= size; ++row)
 	{
 		for (column = 1; column <= size; ++column)
+		{
 			_putchar('#');
-		_putchar('\n');
+		}
+		putchar('\n');
+	}
 	}
 }
