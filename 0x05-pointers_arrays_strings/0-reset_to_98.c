b2b78f8c6662da485b0fca95390e1b6d0c005cd3
EmilioCliff <emiliocliff@gmail.com>
2023-02-28 19:00:31 +0300

    Task 1 on swaping

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - Dereferencing value of int to 98
  *
- * @n - value to be dereference
+ * @n: value to be dereference
  */
 void reset_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers
+ *
+ * @a: first value to be swaped
+ * @b: second value to be swaped
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
