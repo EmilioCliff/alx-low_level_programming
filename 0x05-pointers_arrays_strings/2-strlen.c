1c58cdafe5fac01bcb640d89d0346ffa16e91d7a
EmilioCliff <emiliocliff@gmail.com>
2023-02-28 19:37:29 +0300

    size of a sting

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: the string to determine length
+ *
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(s);
+	return (len);
+}
