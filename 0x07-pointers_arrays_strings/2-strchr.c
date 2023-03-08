19ab2a3d1021013a725c35d3493bc01678bc92ee
EmilioCliff <emiliocliff@gmail.com>
2023-03-08 16:58:00 +0300

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: string to search for character
+ * @c: character to be searched in string
+ *
+ * Return: return s if charachter is located
+ * 			return NULL if character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		break;
+		}
+		else 
+			i++;
+	}
+	return (NULL);
+}
