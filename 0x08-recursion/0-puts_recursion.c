5aa9b82cc7b8e8e0014ac96e5bf3d49b838a165e
EmilioCliff <emiliocliff@gmail.com>
2023-03-09 20:25:42 +0300

    Task 1 fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-	if (s[i] == '\0')
-		return (_putchar(\n));
-
-	_putchar(s[i]);
-		i++;
-		return (_put_recursion(s[i]));
+	if (*s == '\0')
+	{
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
