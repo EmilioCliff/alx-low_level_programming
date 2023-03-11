8c74df3066b37dd549519e4be57b0171b3a4234d
EmilioCliff <emiliocliff@gmail.com>
2023-03-11 21:16:30 +0300

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: String passed to program
  * Return: Always return 0
  */
-int main(int argc, char __attribute__((unused)) *argv)
+int main(int argc, char __attribute__((unused)) **argv)
 {
 	printf("%d\n", argc - 1);
 	return (0);
