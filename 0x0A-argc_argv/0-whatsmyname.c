39d0898218ead2089f385214a71ffb13c5392082
EmilioCliff <emiliocliff@gmail.com>
2023-03-11 20:36:26 +0300

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
 /**
  * main - prints the name it's called with
  * @argc: Number of arguments passed to program
@@ -9,14 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-	char *ch = argv[0];
-	int i = 0;
+	int i;
 
-	while (ch[i] != '\0')
+	for (i = 0; i < argc; i++)
 	{
-		_putchar(ch[i]);
-		i++;
+		printf("%s", argv[i]);
 	}
-	_putchar('\n');
-	return (0);
+	printf("\n");
 }
