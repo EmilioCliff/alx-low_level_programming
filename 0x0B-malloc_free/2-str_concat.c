76b95de3d4a0d5464d781e986ac4d4edd8f4f697
EmilioCliff <emiliocliff@gmail.com>
2023-03-13 20:22:58 +0300

    Task 3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * str_concat - concatenates two string
+ *
+ * @s1: string to be concatenated to
+ * @s2: string to add to another string
+ *
+ * Return: NULL if failure
+ *		pointer if success
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int l1, l2, i;
+	char *ptr;
+
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+	ptr = malloc(sizeof(char) * (l1 + l2 + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < l2; i++)
+	{
+		s1[l1 + i + 1] = s2[i];
+		ptr[i] = s1[i];
+	}
+	return (ptr);
+}
