85d9b0f456682ebb0bd61721224cb4bdbc7c6429
EmilioCliff <emiliocliff@gmail.com>
2023-03-20 20:03:06 +0300

    First commit of struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints the members of struct dog
+ * @d: pointer to stuct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf(" \n");
+	else
+		name = d -> name;
+		age = d -> age;
+		owner = d -> owner;
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Describe a Dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the day
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
