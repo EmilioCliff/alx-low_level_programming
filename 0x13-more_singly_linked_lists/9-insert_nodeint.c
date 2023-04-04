91b2597bac51d332d9d81c629602527f7c82cf61
EmilioCliff <emiliocliff@gmail.com>
2023-04-04 20:35:42 +0300

    Code fix on task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode->n = n;
 	if (idx == 0)
 	{
-		newNode->next = temp->next;
+		newNode->next = *head;
 		*head = newNode;
 	}
 	else if (temp->next)
